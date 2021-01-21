766a2ff0519e28479a8e27925b7f2ad3df946a03
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-01-21 14:30:22 -0300

    Create model

--- train_data.py
+++ train_data.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from model import NeuralNet
 
 with open('intents.json', 'r', encoding='utf-8') as file:
     intents = json.load(file)
@@ -59,9 +60,16 @@ class ChatDataset(Dataset):
 
 # Hyperparameters
 batch_size = 8
+hidden_size = 8
+output_size = len(tags)
+input_size = len(X_train[0])
+print(input_size, len(all_words))
+print(output_size, tags)
 
 dataset = ChatDataset()
 train_loader = DataLoader(dataset=dataset, 
                           batch_size=batch_size,
                           shuffle=True,
                           num_workers=2)
+
+model = NeuralNet(input_size, hidden_size, output_size)
