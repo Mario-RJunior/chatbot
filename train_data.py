a45fd0bde533f9ad0ee31de0aaffc3e5e28172ab
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-01-21 12:05:47 -0300

    Create training dataset

--- train_data.py
+++ train_data.py
@@ -2,6 +2,9 @@ import json
 from nltk_utils import tokenize, stem, bag_of_word
 import string
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
 with open('intents.json', 'r', encoding='utf-8') as file:
     intents = json.load(file)
@@ -39,3 +42,27 @@ for (pattern_sentence, tag) in xy:
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
+
+# Create a class
+class ChatDataset(Dataset):
+
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+    
+    def __getitem__(self, index):
+        return self.x_data[idx], self.y_data[idx]
+
+    def __len__(self):
+        return self.n_samples
+
+# Hyperparameters
+batch_size = 8
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset, 
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=2)
+                          
