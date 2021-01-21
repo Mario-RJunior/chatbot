42247e4a5617d37a7bbf318dbf03cfcf36d956be
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-01-21 11:44:14 -0300

    Create X_train and y_train arrays

--- train_data.py
+++ train_data.py
@@ -1,6 +1,7 @@
 import json
-from nltk_utils import tokenize, stem
+from nltk_utils import tokenize, stem, bag_of_word
 import string
+import numpy as np
 
 with open('intents.json', 'r', encoding='utf-8') as file:
     intents = json.load(file)
@@ -8,7 +9,6 @@ with open('intents.json', 'r', encoding='utf-8') as file:
 all_words = []
 tags = []
 xy = []
-
 
 for intent in intents['intents']:
     tag = intent['tag']
@@ -20,8 +20,22 @@ for intent in intents['intents']:
         xy.append((w, tag))
 
 punctuation = string.punctuation
+
 all_words = [stem(w) for w in all_words if w not in punctuation]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
 print(all_words)
 print(tags)
+
+X_train = []
+y_train = []
+
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_word(pattern_sentence, all_words)
+    X_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
