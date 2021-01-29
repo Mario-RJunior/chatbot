e884547e193ab37668cfd3028346ffa629bd6a48
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-01-29 19:41:28 -0300

    Add 'activate_mic' and 'create_audio' functions

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,6 +1,10 @@
 import nltk
 from nltk.stem.porter import PorterStemmer
 import numpy as np
+import speech_recognition as sr
+from gtts import gTTS
+from playsound import playsound
+import os
 # nltk.download('punkt')
 
 stemmer = PorterStemmer()
@@ -19,3 +23,25 @@ def bag_of_word(tokenized_sentence, all_words):
         if w in tokenized_sentence:
             bag[index] = 1.0
     return bag
+
+def activate_mic():
+    global frase
+    microphone = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        microphone.adjust_for_ambient_noise(source)
+        # print('Microfone...')
+        audio = microphone.listen(source)
+    try:
+        frase = microphone.recognize_google(audio, language='pt-BR')
+        print('Usuário: ', frase)
+    except sr.UnknownValueError:
+        print('Mina: Isso não funcionou!')
+
+    return frase
+
+def create_audio(text):
+    tts = gTTS(text, lang='pt-BR')
+    tts.save('C:/Users/mjuni/OneDrive/Documentos/Junior/Chatbot/bot3.mp3')
+    playsound('C:/Users/mjuni/OneDrive/Documentos/Junior/Chatbot/bot3.mp3')
+    os.remove('C:/Users/mjuni/OneDrive/Documentos/Junior/Chatbot/bot3.mp3')
